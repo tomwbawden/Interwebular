967e150b23b34f0fa21735b4459d8df1a8f86792
Tom Bawden <tommyb@interwebular.com.au>
2012-06-12 01:51:00 +1000

    VC test 1

--- html/index.php
+++ html/index.php
@@ -2,6 +2,7 @@
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<!-- Gitted -->
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<meta name="Description" content="Portfolio and digital playground of Brisbane freelance web designer Tom Bawden.">
 	<title>Interwebular - A Brisbane Web Designer</title>
